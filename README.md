421f7d8c2db4255b639fb06b2654e1e729b68758
Denys Topchyi <miliarder97@gmail.com>
2023-10-05 16:55:01 +0300

    added readme

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+
+# Potr Pots
+
+An eco-friendly, self-watering, origami plant pot website.
+
+## Overview
+
+Potr Pots is an eco-friendly web project showcasing a unique kind of plant pot. These pots are not only good for the environment but are also very functional and aesthetically pleasing.
+
+## Features
+
+- **Eco-Friendly:** All materials used are 100% recycled.
+- **Self-Watering:** Plants self-regulate their water intake using wicking technology.
+- **Easy Assembly:** POTR Pots are simple and satisfying to assemble.
+- **Flat-Pack Postage:** They arrive flat packed for convenience.
+
+## Sections
+
+1. **Header:** Contains site navigation and a beautiful presentation of the POTR Pots product.
+2. **Menu:** Additional navigation and quick links.
+3. **Main Content:** Includes sections about the pot, its features, the team behind it, materials used, size recommendations, and a checkout section.
+4. **Footer:** Contains contact details and social media links.
+
+## IntersectionObserver Module
+
+- Uses the IntersectionObserver API to implement scroll-triggered animations.
+- Elements with the class `animate-on-scroll` will animate when they enter the viewport.
+- The `rootMargin` option in the observer ensures the animations trigger a bit before the element is fully in view.
+
+## Additional Info
+
+- **Styles:** Styled using SCSS (referenced as `main.scss`).
+- **Scripts:** The site uses custom scripts (`main.js` and `dynamicAdapt.js`).
+
+## Contact
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
 (function sliderModule() {
   let slideIndex = 0;
   let touchStartX = 0;
-  const swiperContainer = document.querySelector('.swiper-container');
-  const bullets = document.querySelectorAll('.swiper-item');
+  const swiperContainer = document.querySelector(".swiper-container");
+  const bullets = document.querySelectorAll(".swiper-item");
 
   function showSlides(n) {
-    const slides = document.querySelectorAll('.swiper-slide');
+    const slides = document.querySelectorAll(".swiper-slide");
 
     if (n >= slides.length) {
       slideIndex = 0;
@@ -18,10 +19,10 @@
     }
 
     for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
+      slides[i].style.display = "none";
     }
 
-    slides[slideIndex].style.display = 'block';
+    slides[slideIndex].style.display = "block";
     updateBullets(slideIndex);
   }
 
@@ -33,11 +34,11 @@
     showSlides((slideIndex -= 1));
   }
 
-  swiperContainer.addEventListener('touchstart', (e) => {
+  swiperContainer.addEventListener("touchstart", (e) => {
     touchStartX = e.changedTouches[0].clientX;
   });
 
-  swiperContainer.addEventListener('touchend', (e) => {
+  swiperContainer.addEventListener("touchend", (e) => {
     const touchEndX = e.changedTouches[0].clientX;
 
     if (touchEndX > touchStartX) {
@@ -50,23 +51,23 @@
   function updateBullets(index) {
     bullets.forEach((bullet, bulletIndex) => {
       if (bulletIndex === index) {
-        bullet.classList.add('active');
+        bullet.classList.add("active");
       } else {
-        bullet.classList.remove('active');
+        bullet.classList.remove("active");
       }
     });
   }
 
   function setupBulletClicks() {
     bullets.forEach((bullet, index) => {
-      bullet.addEventListener('click', function() {
+      bullet.addEventListener("click", () => {
         slideIndex = index;
         showSlides(slideIndex);
       });
     });
   }
 
-  window.addEventListener('load', function() {
+  window.addEventListener("load", () => {
     setupBulletClicks();
     updateBullets(slideIndex);
     showSlides(slideIndex);
@@ -74,72 +75,76 @@
 })();
 
 (function menuModule() {
-  window.addEventListener('hashchange', () => {
-    if (window.location.hash === '#menu') {
-      document.body.classList.add('page__body--with-menu');
+  window.addEventListener("hashchange", () => {
+    if (window.location.hash === "#menu") {
+      document.body.classList.add("page__body--with-menu");
     } else {
-      document.body.classList.remove('page__body--with-menu');
+      document.body.classList.remove("page__body--with-menu");
     }
   });
 
-  window.addEventListener('load', function() {
+  window.addEventListener("load", () => {
     const hash = window.location.hash.substring(1);
 
-    if (hash === 'menu') {
-      document.querySelector('.page').classList.add('page__body--with-menu');
+    if (hash === "menu") {
+      document.querySelector(".page").classList.add("page__body--with-menu");
     }
   });
 })();
 
 (function popUpModule() {
-  const popUpBlocks = document.querySelectorAll('.pop-up__block');
+  const popUpBlocks = document.querySelectorAll(".pop-up__block");
 
   const resetPopUpElements = () => {
-    document.querySelectorAll('.pop-up__modal-container')
-      .forEach(e => e.remove());
+    document
+      .querySelectorAll(".pop-up__modal-container")
+      .forEach((e) => e.remove());
 
-    document.querySelectorAll('.pop-up__button')
+    document
+      .querySelectorAll(".pop-up__button")
       // eslint-disable-next-line no-return-assign
-      .forEach(btn => btn.textContent = '+');
+      .forEach((btn) => (btn.textContent = "+"));
 
-    document.querySelectorAll('.pop-up__block')
-      .forEach(blk => blk.classList.remove('active-modal'));
+    document
+      .querySelectorAll(".pop-up__block")
+      .forEach((blk) => blk.classList.remove("active-modal"));
   };
 
   const createModal = (button, block) => {
-    const modalContainer = document.createElement('div');
-    const modal = document.createElement('div');
-    const modalContent = document.createElement('div');
+    const modalContainer = document.createElement("div");
+    const modal = document.createElement("div");
+    const modalContent = document.createElement("div");
 
-    modalContainer.classList.add('pop-up__modal-container');
-    modal.classList.add('pop-up__modal');
-    modalContent.classList.add('pop-up__modal-content');
-    block.classList.add('active-modal');
-    modalContent.textContent = button.getAttribute('data-text');
+    modalContainer.classList.add("pop-up__modal-container");
+    modal.classList.add("pop-up__modal");
+    modalContent.classList.add("pop-up__modal-content");
+    block.classList.add("active-modal");
+    modalContent.textContent = button.getAttribute("data-text");
 
     modal.appendChild(modalContent);
     modalContainer.appendChild(modal);
     block.appendChild(modalContainer);
 
-    modalContainer.style.top
-     = button.getAttribute('data-position-top') + 'px';
+    modalContainer.style.top = button.getAttribute("data-position-top") + "px";
 
-    modalContainer.style.left
-      = button.getAttribute('data-position-left') + 'px';
+    // eslint-disable-next-line operator-linebreak
+    modalContainer.style.left =
+      button.getAttribute("data-position-left") + "px";
 
-    button.textContent = '-';
+    button.textContent = "-";
   };
 
-  popUpBlocks.forEach(function(block) {
-    block.addEventListener('click', function() {
-      const button = block.querySelector('.pop-up__button');
-      const existingModalContainer
-       = block.querySelector('.pop-up__modal-container');
+  popUpBlocks.forEach((block) => {
+    block.addEventListener("click", () => {
+      const button = block.querySelector(".pop-up__button");
+      const existingModalContainer = block.querySelector(
+        ".pop-up__modal-container"
+      );
 
       if (existingModalContainer) {
         existingModalContainer.remove();
-        button.textContent = '+';
-        block.classList.remove('active-modal');
+        button.textContent = "+";
+        block.classList.remove("active-modal");
 
         return;
       }
@@ -151,10 +156,10 @@
 })();
 
 (function formModule() {
-  const form = document.querySelector('.form');
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const messageInput = document.getElementById('message');
+  const form = document.querySelector(".form");
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
 
   const removeClasses = (input, ...classes) => {
     classes.forEach((className) => input.classList.remove(className));
@@ -166,24 +171,25 @@
 
   const validateInput = (input, regex) => {
     if (regex.test(input.value)) {
-      removeClasses(input, 'invalid');
-      addClass(input, 'valid');
+      removeClasses(input, "invalid");
+      addClass(input, "valid");
 
       return true;
     } else {
-      removeClasses(input, 'valid');
-      addClass(input, 'invalid');
+      removeClasses(input, "valid");
+      addClass(input, "invalid");
 
       return false;
     }
   };
 
-  [nameInput, emailInput, messageInput].forEach(input => {
-    input.addEventListener('focus',
-      () => removeClasses(input, 'valid', 'invalid'));
+  [nameInput, emailInput, messageInput].forEach((input) => {
+    input.addEventListener("focus", () =>
+      removeClasses(input, "valid", "invalid")
+    );
   });
 
-  form.addEventListener('submit', (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     // Regular expressions for input fields
@@ -194,15 +200,14 @@
 
     let isFormValid = validateInput(nameInput, regex);
 
-    isFormValid
-      = validateInput(emailInput, emailRegex) && isFormValid;
-    addClass(messageInput, 'valid');
+    isFormValid = validateInput(emailInput, emailRegex) && isFormValid;
+    addClass(messageInput, "valid");
 
     if (isFormValid) {
       form.reset();
 
-      [nameInput, emailInput, messageInput].forEach(
-        input => removeClasses(input, 'valid', 'invalid')
+      [nameInput, emailInput, messageInput].forEach((input) =>
+        removeClasses(input, "valid", "invalid")
       );
     }
   });
@@ -210,25 +215,25 @@
 
 (function IntersectionObserverModule() {
   // eslint-disable-next-line no-undef
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      // Если элемент в поле зрения и еще не был анимирован
-      if (entry.isIntersecting && !entry.target.dataset.animated) {
-        // Добавьте класс для анимации
-        entry.target.classList.add('animate');
-        entry.target.classList.remove('hide');
-        // Отметьте элемент как анимированный
-        entry.target.dataset.animated = true;
-      }
-    });
-  }, {
-    rootMargin: '-10% 0px',
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !entry.target.dataset.animated) {
+          entry.target.classList.add("animate");
+          entry.target.classList.remove("hide");
+          entry.target.dataset.animated = true;
+        }
+      });
+    },
+    {
+      rootMargin: "-10% 0px",
+    }
+  );
 
-  const elements = document.querySelectorAll('.animate-on-scroll');
+  const elements = document.querySelectorAll(".animate-on-scroll");
 
   elements.forEach((element) => {
-    element.classList.add('hide');
+    element.classList.add("hide");
     observer.observe(element);
   });
 })();
